3c212a2155101eff052f9d6c552850cfa1f244a8
locona <locona.0810@gmail.com>
2019-12-11 19:27:36 +0900

    chore: adjust release config

---
+++ .release-rules.js
@@ -0,0 +1,12 @@
+module.exports = [
+  {breaking: true, release: 'major'},
+  {revert: true, release: 'patch'},
+  // Default angular
+  {type: 'feat', release: 'minor'},
+  {type: 'fix', release: 'patch'},
+  {type: 'improvement', release: 'patch'},
+  {type: 'chore', release: 'patch'},
+  {type: 'docs', release: 'patch'},
+  {type: 'refactor', release: 'patch'},
+  {type: 'ci', release: 'patch'}
+];
